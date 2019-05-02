846299bc81c129e7520ed6c9bc695eee2937491b
morganjunlin <morganjunlin@gmail.com>
2019-05-01 17:30:40 -0700

    completed working DB functionality up to models/controllers index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,51 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.connection.connect();
+      
+      var query = db.connection.query('SELECT user, text FROM messages')
+
+      query.on('error', function(err) {
+        throw err;
+      });
+
+      query.on('fields', function(fields) {
+        console.log(fields);
+      });
+
+      query.on('result', function(row) {
+        db.connection.pause();
+
+        // do something here idk what
+        console.log(row);
+
+        db.connection.resume();
+      })
+
+      db.connection.end();
+    }, // a function which produces all the messages
+    post: function () {
+
+      db.connection.connect();
+
+      var query = db.connection.query('INSERT INTO messages VALUES ');
+
+      query.on('error', function(err) {
+        throw err;
+      });
+
+      query.on('fields', function(fields) {
+        console.log(fields);
+      });
+
+      query.on('result', function(result) {
+        console.log(result);
+      });
+
+      db.connection.end();
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
--- server/schema.sql
+++ server/schema.sql
@@ -2,12 +2,31 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT(6) PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT(6) PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30)
+);
 
+CREATE TABLE messages (
+  id INT(6) PRIMARY KEY AUTO_INCREMENT,
+  text VARCHAR(140) NOT NULL,
+  timestamp TIMESTAMP,
+  user INT(6),
+  room INT(6),
+  FOREIGN KEY(user) REFERENCES users(id),
+  FOREIGN KEY(room) REFERENCES rooms(id) 
+);
+
+/* Describe your table here.*/
+DESCRIBE users;
+DESCRIBE rooms;
+DESCRIBE messages;
 
 
 
